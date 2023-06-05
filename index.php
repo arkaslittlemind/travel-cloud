21d3d9af70bc75d21474908e47429b624da94234
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-06-06 02:26:49 +0530

    master

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-<!DOCTYPE php>
-<php lang="en">
+<!DOCTYPE html>
+<html lang="en">
 <head>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -272,4 +272,4 @@
 <script src="js/script.js"></script>
 
 </body>
-</php>
+</html>
